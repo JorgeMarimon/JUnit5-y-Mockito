c392c1ebc555e156d1f7f6ca5fc9023e39737493
jorgemarimon <jorgemarimon88@gmail.com>
2024-09-10 12:42:00 +0200

    Refactorized code using Clock instead of Date and DateFormat

--- src/test/java/org/learning/demo/controllers/UserControllerIntegrationTest.java
+++ src/test/java/org/learning/demo/controllers/UserControllerIntegrationTest.java
@@ -1,62 +1,62 @@
 package org.learning.demo.controllers;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.time.*;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 class UserControllerIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @MockBean
-    Date date;
-
-    @MockBean
-    DateFormat dateFormat;
-
+    Clock clock;
 
     @Test
-    void testGreeting() throws Exception{
-        mockMvc.perform(get("/users/greetings"))
+    void testGreeting() throws Exception {
+        mockMvc.perform(get("/users/greeting"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello, users!"));
     }
 
     @Test
-    void testUniqueGreeting() throws Exception{
+    void testUniqueGreeting() throws Exception {
         mockMvc.perform(get("/users/uniquegreeting?name=Jorge"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello, Jorge!"));
     }
 
-    @Test
-    void testTimeGreeting() throws Exception{
-        DateFormat mockDateFormat = Mockito.mock(DateFormat.class);
+    void testTimeGreeting() throws Exception {
+        Instant instant = ZonedDateTime.of(LocalDateTime.of(2024, 9, 9, 0, 0),
+                ZoneId.systemDefault()).toInstant();
+        when(clock.instant()).thenReturn(instant);
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
 
-        Mockito.when(mockDateFormat.format(Mockito.any(Date.class))).thenReturn("11"); // Mockea la hora para AM
-        mockMvc.perform(get("/users/timegreeting"))
+        mockMvc.perform(get("/users/timegreeting?name=Jorge"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Good morning, Jorge!"));
 
-        Mockito.when(mockDateFormat.format(Mockito.any(Date.class))).thenReturn("13"); // Mockea la hora para AM
-        mockMvc.perform(get("/users/timegreeting"))
+        instant = ZonedDateTime.of(LocalDateTime.of(2024, 9, 9, 12, 0),
+                ZoneId.systemDefault()).toInstant();
+        when(clock.instant()).thenReturn(instant);
+
+        mockMvc.perform(get("/users/timegreeting?name=Jorge"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Good afternoon, Jorge!"));
     }
-
 }
