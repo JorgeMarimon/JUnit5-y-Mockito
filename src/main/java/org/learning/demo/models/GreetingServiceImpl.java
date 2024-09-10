53e3d50fcdd2f6c0bc86075bccdb62568eb8e780
jorgemarimon <jorgemarimon88@gmail.com>
2024-09-10 15:14:10 +0200

    Cleaning code

--- src/main/java/org/learning/demo/models/GreetingServiceImpl.java
+++ src/main/java/org/learning/demo/models/GreetingServiceImpl.java
@@ -1,7 +1,6 @@
 package org.learning.demo.models;
 
 import org.learning.demo.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Clock;
@@ -10,19 +9,11 @@ import java.time.LocalTime;
 @Service
 public class GreetingServiceImpl implements GreetingService {
 
-    //private final DateFormat dateFormat;
-    //private final Date date;
     private final Clock clock;
 
     public GreetingServiceImpl(Clock clock) {
         this.clock = clock;
     }
-
-    /*@Autowired
-    public GreetingServiceImpl(Date date, DateFormat dateFormat) {
-        this.date = date;
-        this.dateFormat = dateFormat;
-    }*/
 
     @Override
     public String getGreeting() {
@@ -37,7 +28,6 @@ public class GreetingServiceImpl implements GreetingService {
     @Override
     public String getTimeGreeting(String name) {
         LocalTime now = LocalTime.now(clock);
-        //double now = Double.parseDouble(dateFormat.format(date));
         if (now.isBefore(LocalTime.NOON)) {
             return "Good morning, " + name + "!";
         } else {
