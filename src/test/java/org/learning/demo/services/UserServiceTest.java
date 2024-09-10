5a4c0a0fda52edc68ed7b60ab4128cd44fbeecf6
jorgemarimon <jorgemarimon88@gmail.com>
2024-09-10 12:03:10 +0200

    Verify included. ExtendWith included.

--- src/test/java/org/learning/demo/services/UserServiceTest.java
+++ src/test/java/org/learning/demo/services/UserServiceTest.java
@@ -1,13 +1,16 @@
 package org.learning.demo.services;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
     private final String RESULT = "Hello, users!";
@@ -23,30 +26,30 @@ class UserServiceTest {
 
     @Test
     public void messageTest() {
-        MockitoAnnotations.openMocks(this);
         when(greetingService.getGreeting()).thenReturn(RESULT);
         assertEquals("Hello, users!", userService.message(), "El mensaje debe ser" + RESULT);
+        verify(greetingService).getGreeting();
     }
 
     @Test
     public void messageUniqueTest() {
-        MockitoAnnotations.openMocks(this);
         when(greetingService.getUniqueGreeting(NAME)).thenReturn(NAME);
         assertEquals("Hello, Jorge!", userService.uniqueGreeting(NAME), "The result should be 'Hello, Jorge!'");
+        verify(greetingService).getUniqueGreeting(NAME);
     }
 
     @Test
     public void messageTimeTest() {
-        MockitoAnnotations.openMocks(this);
         when(greetingService.getTimeGreeting(NAME)).thenReturn(TIME1);
         assertEquals("Good morning, Jorge!", userService.getTimeGreeting(NAME), "The result should be 'Good morning, Jorge!");
+        verify(greetingService).getTimeGreeting(NAME);
     }
 
     @Test
     public void messageTimeTest2() {
-        MockitoAnnotations.openMocks(this);
         when(greetingService.getTimeGreeting(NAME)).thenReturn(TIME2);
         assertEquals("Good afternoon, Jorge!", userService.getTimeGreeting(NAME), "The result should be 'Good afternoon, Jorge!");
+        verify(greetingService).getTimeGreeting(NAME);
     }
 
 }
